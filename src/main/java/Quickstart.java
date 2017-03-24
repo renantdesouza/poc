0c0ab3b3baa4a145fd463eaeeece743aee3cef8a
renan.souza <r34573773>
2017-03-24 15:12:00 -0300

    commit

---
+++ src/main/java/Quickstart.java
@@ -0,0 +1,83 @@
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.sheets.v4.SheetsScopes;
+import com.google.api.services.sheets.v4.model.*;
+import com.google.api.services.sheets.v4.Sheets;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
+public class Quickstart {
+
+    private static final List<String> SCOPES = Arrays.asList(SheetsScopes.SPREADSHEETS);
+
+    private static final String APPLICATION_NAME = "Sheets API";
+
+    private static final java.io.File DATA_STORE_DIR = new java.io.File(System.getProperty("user.home"), ".credentials/sheets.googleapis.com-java-quickstart");
+
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+
+    private static FileDataStoreFactory DATA_STORE_FACTORY;
+
+    private static HttpTransport HTTP_TRANSPORT;
+
+    static {
+        try {
+            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
+        } catch (Throwable t) {
+            t.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public static Credential authorize() throws IOException {
+        InputStream in = Quickstart.class.getResourceAsStream("/client_secret.json");
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow
+                .Builder(HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(DATA_STORE_FACTORY)
+                .setAccessType("offline")
+                .build();
+
+        System.out.println("Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
+        return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+    }
+
+    public static Sheets getSheetsService() throws IOException {
+        return new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, authorize()).setApplicationName(APPLICATION_NAME).build();
+    }
+
+    public static void main(String[] args) throws IOException {
+        String spreadsheetId = "1R9kkICgd6y7T152IvJ4g5yQDnk7NmzKkuwnyw660FwA";
+        String range = "teste!A1:B";
+        List<List<Object>> values = getSheetsService().spreadsheets().values().get(spreadsheetId, range).execute().getValues();
+
+        if (values == null || values.size() == 0) {
+            System.out.println("No data found.");
+            return;
+        }
+
+        for (String tag : entry.getCustomElements().getTags()) {
+            System.out.println("     " + tag + ": " + entry.getCustomElements().getValue(tag));
+        }
+
+        System.out.println("Name, Major");
+        for (List row : values) {
+            System.out.printf("%s, %s\n", row.get(0), row.get(1));
+        }
+    }
+
+}
